b09655781098b35cd1fe3b9a7830d3affeabf101
Marcelo del Rio <mrio@unifiedhealth.com>
2025-05-16 12:41:10 -0400

    chore: set up ESLint + Prettier + Husky

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,31 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import { defineConfig } from 'eslint-define-config';
+import eslintPluginReact from 'eslint-plugin-react';
+import eslintPluginTypescript from '@typescript-eslint/eslint-plugin';
+import parser from '@typescript-eslint/parser';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
+export default defineConfig({
+  languageOptions: {
+    parser: parser,
+    parserOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
+      },
     },
   },
-)
+  plugins: {
+    react: eslintPluginReact,
+    '@typescript-eslint': eslintPluginTypescript,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off', // Para React 17 y superior
+    '@typescript-eslint/no-unused-vars': 'warn',
+  },
+});
+
