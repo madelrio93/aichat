40f512a924e317688a51749899fff020f98e20ac
Marcelo del Rio <mrio@unifiedhealth.com>
2025-05-17 08:31:29 -0400

    chore: install and configure TailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,60 @@
 function App() {
-  return <>App</>;
+  return (
+    <div className="w-3xl h-screen m-auto">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus,
+      veritatis, ea rem eaque incidunt laborum et vitae, expedita voluptatem
+      libero doloribus? Optio repellendus, velit maiores dolorum ratione cum at
+      alias laudantium quod quae, ex, assumenda dolor vel rem. Esse id tenetur
+      aperiam ex, excepturi laborum distinctio architecto pariatur porro, fuga
+      corporis? Iste culpa eum voluptatibus nam totam suscipit commodi dolores a
+      maxime sit eos accusantium minus id nesciunt alias nemo sed enim in
+      delectus harum atque, similique odio. Minus corporis cumque error
+      aspernatur odio necessitatibus, sint optio sed, ducimus adipisci,
+      exercitationem quasi. Sit cumque nulla reiciendis autem! Ab exercitationem
+      in explicabo porro et non, saepe suscipit repellendus, corporis at ad. Vel
+      dolorum distinctio repellat iste eum modi itaque corporis odio saepe
+      labore ipsa dolor architecto ut autem error, quod cupiditate quis
+      reprehenderit illo sunt. Illo, voluptate laborum perferendis incidunt
+      rerum magni voluptatibus veniam veritatis a, non rem omnis quis ullam
+      velit, harum voluptatem soluta? Qui expedita officia obcaecati repellendus
+      cum iure distinctio incidunt iste soluta quaerat. Tenetur, adipisci nulla.
+      Similique exercitationem deserunt nemo corporis reprehenderit enim veniam
+      assumenda alias ipsa laboriosam temporibus obcaecati eveniet velit
+      laudantium vel, itaque cupiditate, officia repellendus esse ab sunt animi
+      ipsam consequatur. Fugiat aperiam illum nobis ex voluptas quos rem
+      voluptates, aspernatur culpa nemo ratione minus ducimus, nostrum
+      consequuntur, delectus eius autem deserunt perspiciatis sed assumenda iure
+      ea quae voluptatem esse. Repudiandae numquam incidunt eum magni aliquam
+      quo quis consectetur similique sed nostrum dicta odit possimus placeat
+      iusto deleniti, adipisci maxime iure qui itaque illo totam minima.
+      Molestiae quod modi commodi laborum ex eos fugit ab dolores quas
+      consequatur libero corrupti, necessitatibus nulla officia sit asperiores
+      placeat qui, tempore ratione at maxime? Eum hic non illo tempora a odit
+      error, sequi asperiores incidunt corrupti quam aliquam recusandae quo?
+      Cumque ipsa veniam quas ex accusantium explicabo? Qui doloremque harum
+      totam voluptate iure in nobis iusto sed itaque, nesciunt officia, mollitia
+      consectetur consequuntur quos atque. Suscipit, quidem accusantium. Iste,
+      veniam officiis nemo aut voluptatem quisquam quidem odio ipsam omnis unde
+      eos ipsa maiores autem saepe tenetur et quibusdam explicabo, delectus,
+      harum corporis excepturi blanditiis at quod nobis. Alias molestiae, ab
+      porro quia adipisci ducimus recusandae hic similique consectetur ad
+      nesciunt, libero dolores est, tempora deleniti veritatis maiores
+      asperiores delectus omnis maxime voluptas in quo reiciendis. Maxime ullam
+      beatae sapiente quaerat obcaecati, officiis praesentium sed a, commodi
+      asperiores molestias? Explicabo officia eum quos ex fugit. Qui, quod
+      maiores dolore architecto accusamus ut nobis dolorum! Similique officiis
+      minima optio asperiores molestiae, numquam vel repellat recusandae placeat
+      magnam quas laudantium temporibus maiores reiciendis libero voluptas
+      aspernatur aliquid iste rerum veritatis at earum. Quod possimus sit
+      tenetur, a deleniti eum neque itaque facilis, labore dignissimos provident
+      veniam totam, dolores at molestiae velit unde animi aut deserunt minus.
+      Molestiae vel sunt tempore in provident quis error aliquid. Error impedit
+      animi earum quas ullam explicabo ratione laudantium consequuntur,
+      doloremque vitae dolorem reiciendis veniam, cumque nobis ad accusantium
+      sequi ea similique maiores. Deleniti provident quae inventore adipisci
+      mollitia voluptatibus vero accusamus incidunt, cumque id.
+    </div>
+  );
 }
 
 export default App;
